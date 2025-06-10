f37d7b1766e5fb79b7b8bae70d55a7affccd3b6e
HYT32513 <nkjun090513@kakao.com>
2025-06-10 14:39:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("첫 aap")
 st.write("hello")
+name=st.text_input("이름 입력")
+if name:
+  if name=="hayate"
+  st.success("반갑습니다. hayate님!")
+else:
+  st.warning("누구세요?")
